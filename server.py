4c4fb9a16adddd438b0dd52d742b811eecb21a4e
idarousse <idarousse.i@hotmail.fr>
2023-01-17 01:59:16 +0100

    Ajout de la redirection si la connection n'est pas autorisé

--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
-
-
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
-
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+from http import HTTPStatus
 
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
+
+
+def loadClubs():
+    with open('clubs.json') as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
+
+
+def loadCompetitions():
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
+
 
 competitions = loadCompetitions()
 clubs = loadClubs()
@@ -26,9 +28,12 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
-
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except IndexError:
+        flash("L'email est incorrect.")
+        return redirect(url_for("index")), HTTPStatus.UNAUTHORIZED
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
