629551e5b7d1293f5578cac65e46ff11dfe11294
idarousse <idarousse.i@hotmail.fr>
2023-01-18 22:01:57 +0100

    correction test et respect de la pep8

--- tests/test_integration/test_authentication.py
+++ tests/test_integration/test_authentication.py
@@ -1,29 +1,28 @@
 import pytest
 from http import HTTPStatus
-from server import app
-from flask import flash
 
 HTTP_OK = HTTPStatus.OK
-HTTP_UNAUTHORIZED = HTTPStatus.UNAUTHORIZED
+HTTP_FOUND = HTTPStatus.FOUND
 ENDPOINT_SHOWSUMMARY = "/showSummary"
 
+
 class TestAuthenticationEmail:
-    
-    def test_should_status_code_ok(self,client):
-        response = client.get('/')
+    def test_should_status_code_ok(self, client):
+        response = client.get("/")
         assert response.status_code == HTTP_OK
 
-
-    @pytest.mark.parametrize("email, status_code", [
-        ({'email': 'admin@irontemple.com'}, HTTP_OK),
-        ({'email': 'bad@mail.com'}, HTTP_UNAUTHORIZED)])
-    def test_authenticate(self,client,email,status_code):
-        response= client.post(ENDPOINT_SHOWSUMMARY, data=email)
-        if response.status_code == HTTP_OK:
-            message = f"Welcome, {email['email']}"
-            assert response.status_code == status_code
-            assert message in response.data.decode()
-        elif response.status_code == HTTP_UNAUTHORIZED:
-            assert response.status_code == status_code
-            assert "Redirecting" in response.data.decode()
-
+    @pytest.mark.parametrize(
+        "email, status_code, message",
+        [
+            (
+                {"email": "admin@irontemple.com"},
+                HTTP_OK,
+                "Welcome, admin@irontemple.com",
+            ),
+            ({"email": "bad@mail.com"}, HTTP_FOUND, "Redirecting"),
+        ],
+    )
+    def test_authenticate(self, client, email, status_code, message):
+        response = client.post(ENDPOINT_SHOWSUMMARY, data=email)
+        assert response.status_code == status_code
+        assert message in response.data.decode()
