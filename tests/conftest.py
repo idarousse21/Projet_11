85b70c684cef2bfc517d82c6b136e05497b265f0
idarousse <idarousse.i@hotmail.fr>
2023-01-22 16:24:11 +0100

    ajout test d'intégration

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,64 @@ import pytest
 from server import app
 import server
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
 
+@pytest.fixture
+def data_users():
+    data_user = [
+        {
+            "name": "test_club_1",
+            "email": "test1@club.com",
+            "points": "25",
+        },
+        {
+            "name": "test_club_2",
+            "email": "test2@club.com",
+            "points": "3",
+        },
+        {
+            "name": "test_club_3",
+            "email": "test3@club.com",
+            "points": "10",
+        },
+    ]
+    return data_user
+
+
+@pytest.fixture
+def data_competitions():
+    data_user = [
+        {
+            "name": "test_competition_1",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13",
+        },
+        {
+            "name": "test_competition_2",
+            "date": "2020-10-22 13:20:00",
+            "numberOfPlaces": "5",
+        },
+    ]
+    return data_user
+
+
+@pytest.fixture
+def data_base_mocker(mocker, data_users, data_competitions):
+    mocker.patch.object(server, "clubs", data_users)
+    mocker.patch.object(server, "competitions", data_competitions)
+
+
+@pytest.fixture
+def message_response():
+    message = {
+        "less_zero": "The number of seats purchased cannot be less than 1",
+        "less_club_point": "The club points balance is lower than the places bought.",
+        "less_competition_point": "The places to ask for are superior to the places available in the competition.",
+        "purchases_valid": "Great-booking complete!",
+    }
+    return message
--- tests/test_unit/test_function_valid_purchase.py
+++ tests/test_unit/test_function_valid_purchase.py
@@ -2,18 +2,38 @@ import pytest
 from server import valid_purchase
 
 
-
 class TestValidPurchase:
-
-    @pytest.mark.parametrize("places_required,number_places_competition, number_places_club,boolean, message",
+    @pytest.mark.parametrize(
+        "places_required,number_places_competition, number_places_club,boolean, message",
         [
-            (2,25,4,True,"Great-booking complete!"),
-            (13,25,50,False,"You cannot reserve more than 12 places in a competition."),
-            (10,50,5,False,"The club's points balance is lower than the places bought."),
-            (0,10,7,False,"The number of seats purchased cannot be less than 1"),
-            (10,5,70,False,"The places to ask for are superior to the places available in the competition.")])
-    def test_valid_purchase(self,places_required,number_places_competition, number_places_club,boolean,message):
-        purchase = valid_purchase(places_required, number_places_competition, number_places_club)
+            (2, 25, 4, True, "Great-booking complete!"),
+            (
+                10,
+                50,
+                5,
+                False,
+                "The club points balance is lower than the places bought.",
+            ),
+            (0, 10, 7, False, "The number of seats purchased cannot be less than 1"),
+            (
+                10,
+                5,
+                70,
+                False,
+                "The places to ask for are superior to the places available in the competition.",
+            ),
+        ],
+    )
+    def test_valid_purchase(
+        self,
+        places_required,
+        number_places_competition,
+        number_places_club,
+        boolean,
+        message,
+    ):
+        purchase = valid_purchase(
+            places_required, number_places_competition, number_places_club
+        )
         assert purchase[0] == boolean
         assert message == purchase[1]
-
