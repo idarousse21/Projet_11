c2747246fec6fe62e1262816a3c8687e5c378d27
idarousse <idarousse.i@hotmail.fr>
2023-01-25 00:04:43 +0100

    ajout du driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,17 @@ import pytest
 from server import app
 from selenium import webdriver
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def message():
+    data_message = {"invalid": "The email is incorrect.", "valid": "Welcome, "}
+    return data_message
 
 @pytest.fixture
 def driver():
