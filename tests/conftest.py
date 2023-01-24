d5f159bc8790f7738011fff35d3f17f7577108b3
idarousse <idarousse.i@hotmail.fr>
2023-01-24 23:47:18 +0100

    ajout du driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from server import app
-import server
+from selenium import webdriver
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("http://127.0.0.1:5000/")
+    yield driver
+    driver.close()
