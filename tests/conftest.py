1c2449966be6efcf4706309725425e4f7a2acea6
idarousse <idarousse.i@hotmail.fr>
2023-01-24 23:37:08 +0100

    ajout du driver dans le fichier

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,15 @@
 import pytest
 from server import app
 import server
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
 
 
 @pytest.fixture
 def data_users():
-    data_user = [
+    data_users = [
         {
             "name": "test_club_1",
             "email": "test1@club.com",
@@ -28,12 +26,12 @@ def data_users():
             "points": "10",
         },
     ]
-    return data_user
+    return data_users
 
 
 @pytest.fixture
 def data_competitions():
-    data_user = [
+    data_competitions = [
         {
             "name": "test_competition_1",
             "date": "2020-10-22 13:30:00",
@@ -45,7 +43,7 @@ def data_competitions():
             "numberOfPlaces": "5",
         },
     ]
-    return data_user
+    return data_competitions
 
 
 @pytest.fixture
@@ -63,3 +61,19 @@ def message_response():
         "purchases_valid": "Great-booking complete!",
     }
     return message
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("http://127.0.0.1:5000/")
+    user_mail = "admin@irontemple.com"
+    driver.find_element(By.TAG_NAME, "input").send_keys(user_mail)
+    driver.find_element(By.TAG_NAME, "button").click()
+    yield driver
+    driver.close()
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
