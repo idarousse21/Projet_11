d62794eca6574279212b6a81bdba72f4fa9e3712
idarousse <idarousse.i@hotmail.fr>
2023-01-25 19:41:55 +0100

    correction du bug

--- server.py
+++ server.py
@@ -1,59 +1,111 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
+from http import HTTPStatus
 
 
 def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+    with open("clubs.json") as c:
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+    with open("competitions.json") as comps:
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
+
+
+def dictionary_purchases(clubs, competitions):
+    clubs_purchase = {}
+    for club in clubs:
+        clubs_purchase[club["email"]] = {}
+        for competition in competitions:
+            clubs_purchase[club["email"]][competition["name"]] = 0
+    return clubs_purchase
+
+
+def dictionary_purchases_update(clubs_purchase, club_email, competition, purchase):
+    clubs_purchase[club_email][competition] += purchase
+    return clubs_purchase
 
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = "something_special"
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+clubs_purchase = dictionary_purchases(clubs, competitions)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route("/showSummary", methods=["POST"])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club = [club for club in clubs if club["email"] == request.form["email"]][0]
+    return render_template(
+        "welcome.html",
+        club=club,
+        competitions=competitions,
+        club_competition_purchases=clubs_purchase,
+    )
 
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+@app.route("/book/<competition>/<club>")
+def book(competition, club):
+    foundClub = [c for c in clubs if c["name"] == club][0]
+    foundCompetition = [c for c in competitions if c["name"] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template(
+            "booking.html", club=foundClub, competition=foundCompetition
+        )
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template(
+            "welcome.html",
+            club=club,
+            competitions=competitions,
+            club_competition_purchases=clubs_purchase,
+        )
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
+    club = [c for c in clubs if c["name"] == request.form["club"]][0]
+    placesRequired = int(request.form["places"])
+    purchases_club = clubs_purchase[club["email"]][competition["name"]]
+    new_purchases_club = int(purchases_club) + placesRequired
+    if new_purchases_club <= 12:
+        competition["numberOfPlaces"] = (
+            int(competition["numberOfPlaces"]) - placesRequired
+        )
+        dictionary_purchases_update(
+            clubs_purchase, club["email"], competition["name"], placesRequired
+        )
+        flash("Great-booking complete!")
+        return render_template(
+            "welcome.html",
+            club=club,
+            competitions=competitions,
+            club_competition_purchases=clubs_purchase,
+        )
+
+    else:
+        flash("You cannot buy more than 12 places for this competition.")
+        return (
+            render_template("booking.html", club=club, competition=competition),
+            HTTPStatus.BAD_REQUEST,
+        )
 
 
 # TODO: Add route for points display
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,64 @@ import pytest
 from server import app
 import server
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
 
+@pytest.fixture
+def data_users():
+    data_user = [
+        {
+            "name": "test_club_1",
+            "email": "test1@club.com",
+            "points": "25",
+        },
+        {
+            "name": "test_club_2",
+            "email": "test2@club.com",
+            "points": "3",
+        },
+        {
+            "name": "test_club_3",
+            "email": "test3@club.com",
+            "points": "10",
+        },
+    ]
+    return data_user
+
+
+@pytest.fixture
+def data_competitions():
+    data_competition = [
+        {
+            "name": "test_competition_1",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13",
+        },
+        {
+            "name": "test_competition_2",
+            "date": "2020-10-22 13:20:00",
+            "numberOfPlaces": "5",
+        },
+    ]
+    return data_competition
+
+
+@pytest.fixture
+def data_purchases_clubs():
+    data_purchases_clubs = {
+            "test1@club.com": {"test_competition_1": 0, "test_competition_2": 0},
+            "test2@club.com": {"test_competition_1": 0, "test_competition_2": 0},
+            "test3@club.com": {"test_competition_1": 0, "test_competition_2": 0},
+        }
+    return data_purchases_clubs
+
+
+@pytest.fixture
+def data_base_mocker(mocker, data_users, data_competitions,data_purchases_clubs):
+    mocker.patch.object(server, "clubs", data_users)
+    mocker.patch.object(server, "competitions", data_competitions)
+    mocker.patch.object(server, "clubs_purchase", data_purchases_clubs)
