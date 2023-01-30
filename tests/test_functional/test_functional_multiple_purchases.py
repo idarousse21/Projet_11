0e279ae79065e3f832bb64c8f3a91a97eb18466c
idarousse <idarousse.i@hotmail.fr>
2023-01-30 22:24:36 +0100

    Ajout du test functionel

---
+++ tests/test_functional/test_functional_multiple_purchases.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+import pytest
+import random
+import time
+
+
+def function_purchases(driver, number_places):
+    time.sleep(4)
+    driver.find_element(By.LINK_TEXT, "Book Places").click()
+    driver.find_element(By.CSS_SELECTOR, "input[name='places']").send_keys(
+        number_places
+    )
+    driver.find_element(By.TAG_NAME, "button").click()
+    return driver
+
+
+class TestMultiplePurchase:
+    def test_multiple_purchase_valid(self, driver):
+        purchases = 4
+        club_purchase = 0
+        while club_purchase < 12:
+            club_purchase += purchases
+            response = function_purchases(driver, purchases)
+        message = f"Purchases club: {str(club_purchase)}"
+        assert message in response.find_elements(By.TAG_NAME, "li")[1].text
+
+    def test_multiple_purchase_invalid(self, driver):
+        purchases = 5
+        club_purchase = 0
+        for _ in range(3):
+            club_purchase += purchases
+            response = function_purchases(driver, purchases)
+        message = "You cannot buy more than 12 places for this competition."
+        assert message == response.find_elements(By.TAG_NAME, "li")[
+            0
+        ].text or not response.find_element(By.LINK_TEXT, "Book Places")
