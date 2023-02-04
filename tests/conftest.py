ff8c82462d35f847f400eba12918ade10346cdcc
idarousse <idarousse.i@hotmail.fr>
2023-02-04 22:50:49 +0100

    ajout de la fonction implicitly_wait

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ def client():
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
     driver.get("http://127.0.0.1:5000/")
     yield driver
     driver.close()
